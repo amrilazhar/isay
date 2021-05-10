39a6f967b4a2d1fcb7d68854b6c5c4c1f62dd8b3
amril <amrilazhar@gmail.com>
2021-05-10 23:16:59 +0700

    fix typo

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -8,13 +8,13 @@ const profileController = require("../controllers/profileController");
 const profileValidator = require("../middlewares/validators/profileValidator");
 
 let authDummy = (req, res, next) => {
-    req.profile = { id: "608c19b85a4b0b19ccced595" };
+    req.profile = { id: "608ac628c8d0a1bfded19469" };
     
     next();
   };
 
 // SET ROUTER PROFILE HERE
-router.get("/getProfile/:id",autDummy, profileController.myProfile);
+router.get("/getProfile/:id",authDummy, profileController.myProfile);
 router.get("/Post",authDummy, profileController.myProfilePost);
 router.get("/Activities", authDummy,profileController.myProfileActivities);
 router.put("/:id",authDummy, profileValidator.profileValidate,profileController.profileUpdate);

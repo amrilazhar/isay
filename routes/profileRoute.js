340d8391b687feae260195cc14ba786bcb62a1a0
amril <amrilazhar@gmail.com>
2021-05-10 23:29:21 +0700

    fix route error in chat and profile

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-// IMPORT HERE 
+// IMPORT HERE
 const profileController = require("../controllers/profileController");
 
 //IMPORT MIDDLEWARE
 const profileValidator = require("../middlewares/validators/profileValidator");
 
 let authDummy = (req, res, next) => {
-    req.profile = { id: "608ac628c8d0a1bfded19469" };
-    
-    next();
-  };
+	req.profile = { id: "608ac628c8d0a1bfded19469" };
+
+	next();
+};
 
 // SET ROUTER PROFILE HERE
 router.get("/getProfile/:id",authDummy, profileController.myProfile);
@@ -20,26 +20,5 @@ router.get("/Activities", authDummy,profileController.myProfileActivities);
 router.put("/:id",authDummy, profileValidator.profileValidate,profileController.profileUpdate);
 router.put("/Interest/:id",authDummy, profileController.addInterest);
 router.put("/Interest/:id",authDummy, profileController.deleteInterest);
-
-// SET ROUTER LOCATION HERE
-router.get("/getLocation", locationController.getAllLocation);
-router.post("/createLocation", locationController.createLocation);
-router.put("/updateLocation/:id", locationController.updateLocation);
-router.delete("/deleteLocation/:id", locationController.deleteLocation);
-
-// SET ROUTER PROFILE HERE
-router.get("/getProfile/:id", profileController.myProfile);
-router.get("/getActProfile", profileController.myProfilePost);
-router.get("/getPostProfile", profileController.myProfileActivities);
-router.put(
-  "/updateProfile/:id",
-  profileValidator.profileValidate,
-  profileController.profileUpdate
-);
-router.put("/AddInterest/:id", profileController.addInterest);
-router.put("/AddLocation/:id", profileController.addLocation);
-router.delete("/delInterest/:id", profileController.deleteInterest);
-router.delete("/delLocation/:id", profileController.deleteLocation);
-router.get("/getListUserInterest", authDummy, profileController.getListUserInterest);
 
 module.exports = router;
